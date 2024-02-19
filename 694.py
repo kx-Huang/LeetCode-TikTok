a58d9a740b94a0f716f524e03d0397ab2f468340
Kexuan Huang <hkx@Kexuans-MacBook-Pro.local>
2024-02-19 18:26:44 -0500

    update: 694

--- 694.py
+++ 694.py
@@ -1,16 +1,16 @@
 def numDistinctIslands(self, grid: List[List[int]]) -> int:
     def dfs(start, shape, i, j):
-        if i >= 0 and j >= 0 and i < len(grid) and j < len(grid[0]) and grid[i][j] == 1:
+        if  0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == 1:
             # track shape using starting coords
             shape.append((i-start[0], j-start[1]))
-            grid[i][j] = 0  # make sure we don't visit again
+            grid[i][j] = 2  # make sure we don't visit again
             dfs(start, shape, i+1, j)
             dfs(start, shape, i-1, j)
             dfs(start, shape, i, j+1)
             dfs(start, shape, i, j-1)
         return shape
 
-    unique_islands, islands = set(), 0
+    unique_islands, ans = set(), 0
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
@@ -19,5 +19,5 @@ def numDistinctIslands(self, grid: List[List[int]]) -> int:
                 island = tuple(dfs((i, j), [], i, j))
                 if island not in unique_islands:
                     unique_islands.add(island)
-                    islands += 1
-    return islands
+                    ans += 1
+    return ans
