57c698454b77c6c6a93ac5d41b5470b32dff7a75
Kexuan Huang <hkx@Kexuans-MacBook-Pro.local>
2024-02-20 00:51:01 -0500

    add: 418

---
+++ 418.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordsTyping(self, sentence: List[str], rows: int, cols: int) -> int:
+        for word in sentence:
+            if len(word) > cols:
+                return 0
+
+        s = ' '.join(sentence) + ' '
+        n = len(s)
+        ptr = 0
+        for i in range(rows):
+            ptr += (cols-1)
+            # end of col fall on space
+            if s[ptr % n] == ' ':
+                ptr += 1
+            # end of col fall on end of word
+            elif s[(ptr+1) % n] == ' ':
+                ptr += 2
+            # at the mid of the word
+            else:
+                while ptr > 0 and s[ptr % n] != ' ':
+                    ptr -= 1
+                ptr += 1
+        return ptr // n
