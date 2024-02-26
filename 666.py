6ee3819d80cde5b7b1fffd5bd2209eb60c8e0eca
Kexuan Huang <hkx@Kexuans-MacBook-Pro.local>
2024-02-25 23:31:35 -0500

    add: 666

--- 666.py
+++ 666.py
@@ -1,15 +1,14 @@
+from collections import defaultdict
+
 class Solution:
     def pathSum(self, nums: List[int]) -> int:
-        dic = collections.defaultdict(int)
+        dic = defaultdict(int)
         for num in nums:
             depth, pos = num//100, (num//10) % 10
             val = num % 10
             dic[depth, pos] += dic[depth-1, (pos+1)//2] + val
-            # dic[(depth, pos)] = dic.get((depth, pos), 0) + dic.get((depth-1, (pos+1)//2), 0) + val
-        print(dic)
         res = 0
         for depth, pos in dic.keys():
             if (depth+1, pos*2-1) not in dic and (depth+1, pos*2) not in dic:
                 res += dic[depth, pos]
-                # res += dic[(depth, pos)]
         return res
